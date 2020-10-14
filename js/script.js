68a75b570547d5bfffb3b2e2365f0ec9cee54de2
Konrad Smuga <smuggk@gmail.com>
2020-10-14 11:28:05 +0200

    adding active to the current link

--- js/script.js
+++ js/script.js
@@ -39,12 +39,11 @@ const titleClickHandler = function (event) {
     console.log('href' , articleSelector);
     
     
-
     /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
     /* add class 'active' to the correct article */
-    
+    targetArticle.classList.add('active');
 
 }
 
