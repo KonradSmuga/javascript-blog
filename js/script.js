90ae5354796766d788a355c67bb5a9debc9c5429
unknown <smuggk@gmail.com>
2020-10-19 04:48:19 -0700

    clicked element, add active class

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-   
- 
+
+
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    
+
 
     /* [DONE] add class 'active' to the clicked link */
 
@@ -25,7 +25,7 @@ const titleClickHandler = function (event) {
 
     /* [DONE] remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.active');
+    const activeArticles = document.querySelectorAll('.post.active');
 
     for (let activeArticle of activeArticles) {
 
@@ -36,9 +36,9 @@ const titleClickHandler = function (event) {
 
     /* get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
-    console.log('href' , articleSelector);
-    
-    
+    console.log('href', articleSelector);
+
+
     /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
