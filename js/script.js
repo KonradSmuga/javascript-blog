6d7d2da4ab427aafbdf983c537d623209fa37c58
unknown <smuggk@gmail.com>
2020-10-19 09:58:39 -0700

    fixing clickable articles

--- js/script.js
+++ js/script.js
@@ -46,48 +46,47 @@
         targetArticle.classList.add('active');
     }
 
-    const links = document.querySelectorAll('.titles a');
-
-    for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
-    }
+  
 
     const optArticleSelector = '.post',
         optTitleSelector = '.post-title',
         optTitleListSelector = '.titles';
-      
-  
 
     const generateTitleList = function () {
-       
+
         /* remove contents of titleList */
-        const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+        const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML ='';
 
         /* for each article */
 
         const articles = document.querySelectorAll(optArticleSelector);
 
-        let html= '';
+        let html = '';
         for (let article of articles) {
             /* get the article id */
-            const articleId = article.getAttribute('.id')
+            const articleId = article.getAttribute('id')
             /* find the title element */
-             /* get the title from the title element */
+            /* get the title from the title element */
             const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-             /* create HTML of the link */
-            const linkHTML = '<li><a href="#' +articleId+'"><span>'+articleTitle+'</span></a></li>';
+            /* create HTML of the link */
+            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-                /* insert link into 
-        
-        titleList */
+            /* insert link into 
+    titleList */
             html = html + linkHTML;
-            
         }
 
-        
-            titleList.innerHTML = html;
+
+        titleList.innerHTML = html;
     }
 
     generateTitleList();
+
+    const links = document.querySelectorAll('.titles a');
+
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
 
 }
